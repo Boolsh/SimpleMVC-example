7efc15b639988c1d9af7ac31c5a0e5e52e557837
Boolsh <pekshin05@mail..ru>
2025-12-05 12:50:00 +0300

    edit user edit

--- application/config/web-local-example.php
+++ application/config/web-local-example.php
@@ -7,9 +7,9 @@
 $config = [
     'core' => [ // подмассив используемый самим ядром фреймворка
         'db' => [ // подмассив конфигурации БД
-            'dns' => 'mysql:host=localhost;dbname=smvcbase',
-            'username' => 'root',
-            'password' => '1234'
+            'dns' => 'mysql:host=localhost;dbname=mvcDB',
+            'username' => 'dura',
+            'password' => '123qwerosso'
         ]
     ]    
 ];
